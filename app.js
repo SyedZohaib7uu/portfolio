01c1ff78cd3f4b8a281a456310e2847b9dfff75b
SyedZohaib7uu <154689818+SyedZohaib7uu@users.noreply.github.com>
2024-06-26 09:40:47 -0700

    nav done

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var tl= gsap.timeline({paused:true})
 tl.to("#full",{
     right:"0",
-    duration:0.8
+    duration:1.2
 })
 tl.from("#full h4" ,{
 x:150
-,duration:0.8,
-stagger:0.2,
+,duration:1.2,
+stagger:0.4,
 opacity:0
 })
 tl.from("#full i",{
@@ -14,8 +14,14 @@ tl.from("#full i",{
 })
 tl.pause()
 document.getElementById("menu").addEventListener("click",()=>{
+    var full = document.getElementById("full")
     tl.play()
+    full.style.display="block"
 })
 document.getElementById("clo").addEventListener("click",()=>{
     tl.reverse()
+    setTimeout(() => {
+        
+        full.style.display="none"
+    }, 5000);
 })
