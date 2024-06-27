329c8c24e7a9f4fba4d3f4ef90c2ca6bc98fd41e
SyedZohaib7uu <154689818+SyedZohaib7uu@users.noreply.github.com>
2024-06-27 04:56:43 -0700

    home pg complete slide remian

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var tl= gsap.timeline({paused:true})
 tl.to("#full",{
     right:"0",
-    duration:1.2
+    duration:0.8
 })
 tl.from("#full h4" ,{
 x:150
-,duration:1.2,
-stagger:0.4,
+,duration:0.8,
+stagger:0.2,
 opacity:0
 })
 tl.from("#full i",{
@@ -20,8 +20,34 @@ document.getElementById("menu").addEventListener("click",()=>{
 })
 document.getElementById("clo").addEventListener("click",()=>{
     tl.reverse()
-    setTimeout(() => {
-        
-        full.style.display="none"
-    }, 5000);
+    
 })
+    var main= document.querySelector(".main")
+var cur = document.querySelector(".curp")
+var sechome = document.querySelector(".overlay")
+main.addEventListener("mousemove",(det)=>{
+gsap.to(cur,{
+    x:det.x,
+    y:det.y,
+    duration:0.3,
+    ease: "back.out(1.7)",
+})
+})
+sechome.addEventListener("mouseenter",()=>{
+    gsap.to(cur,{
+        scale:2
+    })
+})
+sechome.addEventListener("mouseleave",()=>{
+    gsap.to(cur,{
+        scale:1
+    })
+})
+document.querySelectorAll('.di').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+      document.getElementById('marquee').classList.add('paused');
+    });
+    item.addEventListener('mouseleave', () => {
+      document.getElementById('marquee').classList.remove('paused');
+    });
+  });
