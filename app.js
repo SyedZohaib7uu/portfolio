e4fa4348eadb8d8ee44618980dea2d9e37d3273f
SyedZohaib7uu <154689818+SyedZohaib7uu@users.noreply.github.com>
2024-06-27 10:22:14 -0700

    slider done about page remain

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('.main'),
+    smooth: true
+});
+
+
 var tl= gsap.timeline({paused:true})
 tl.to("#full",{
     right:"0",
-    duration:0.8
+    duration:1.6,
+    ease: "expo.out",
 })
 tl.from("#full h4" ,{
 x:150
 ,duration:0.8,
 stagger:0.2,
-opacity:0
+opacity:0,
 })
 tl.from("#full i",{
-    opacity:0
+    opacity:0,
+   
 })
 tl.pause()
 document.getElementById("menu").addEventListener("click",()=>{
@@ -27,27 +35,20 @@ var cur = document.querySelector(".curp")
 var sechome = document.querySelector(".overlay")
 main.addEventListener("mousemove",(det)=>{
 gsap.to(cur,{
-    x:det.x,
-    y:det.y,
+    x:det.x+50+"px",
+    y:det.y+50+"px",
     duration:0.3,
-    ease: "back.out(1.7)",
+   ease: "bounce.out",
+
 })
 })
 sechome.addEventListener("mouseenter",()=>{
     gsap.to(cur,{
-        scale:2
+        scale:3,
     })
 })
 sechome.addEventListener("mouseleave",()=>{
     gsap.to(cur,{
-        scale:1
+        scale:1,
     })
 })
-document.querySelectorAll('.di').forEach(item => {
-    item.addEventListener('mouseenter', () => {
-      document.getElementById('marquee').classList.add('paused');
-    });
-    item.addEventListener('mouseleave', () => {
-      document.getElementById('marquee').classList.remove('paused');
-    });
-  });
